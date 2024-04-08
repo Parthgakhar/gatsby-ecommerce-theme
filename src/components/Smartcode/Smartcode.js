3504f92a9d07b9a126ca7263be60ac969d0e81ed
Parthgakhar <60871920+Parthgakhar@users.noreply.github.com>
2024-04-08 12:44:36 +0530

    Update Smartcode.js

--- src/components/Smartcode/Smartcode.js
+++ src/components/Smartcode/Smartcode.js
@@ -1,4 +1,6 @@
-const Smartcode = () => {
+import React from 'react';
+
+const Smartcode = (props) => {
 return (
     <>
     <link rel="preconnect" href="https://dev.visualwebsiteoptimizer.com" />
